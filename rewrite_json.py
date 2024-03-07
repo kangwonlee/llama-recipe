a385b3914caa131f45f394eb8932872c3cc75638
KangWon LEE <kangwon@gmail.com>
2024-03-07 14:12:39 +0900

    remove id hashes from ipynb

---
+++ rewrite_json.py
@@ -0,0 +1,24 @@
+import json
+import pathlib
+
+
+def main():
+    folder = pathlib.Path(__file__).parent.resolve()
+
+    for file in folder.glob('*.ipynb'):
+        data = json.loads(file.read_text(encoding='utf-8'))
+
+        # remove ipynb id and metadata
+        for cell in data['cells']:
+            if 'id' in cell:
+                del cell['id']
+            if 'metadata' in cell:
+                if 'id' in cell['metadata']:
+                    del cell['metadata']['id']
+
+        with open(file, 'w') as f:
+            json.dump(data, f, indent=4)
+
+
+if "__main__" == __name__:
+    main()
